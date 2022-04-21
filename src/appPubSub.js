4c9074d77a3dc5baf008503eb3448da9828640a4
juan cardenas <juanpabloufps@ufps.edu.co>
2022-04-21 13:00:44 -0500

    pub sub try and extra endpoints

---
+++ src/appPubSub.js
@@ -0,0 +1,27 @@
+const express = require(`express`);
+const { PubSub } = require('@google-cloud/pubsub');
+
+const pubsubClient = new PubSub();
+const app = express
+app.use(express.urlencoded)
+app.use(express.json())
+const port= process.env.PORT ||8080;
+
+app.listen(port,()=>{
+
+    console.log("listening on port",port);
+});
+app.post('/',async(req,res)=>{
+try{
+    const notice=req.body;
+    await publishPubSubMessage(notice);
+    res.status(204).send();
+}catch(ex){
+        console.log(ex);
+        res.status(500).send(ex);
+    }
+});
+async function publishPubSubMessage(notice){
+    const buffer=Buffer.from(JSON.stringify(notice));
+    await pubsub.topic('new-notice').publish(buffer);
+}
--- src/servicios/MediaStackNews.js
+++ src/servicios/MediaStackNews.js
@@ -68,9 +68,9 @@ endpoints.get('/media', async(req, res)=> {
      let filter=[datos.length];
      for(let i=0;i<datos.length;i++){  
         filter[i]= {
-       "author":datos[i].author ,
+       //"author":datos[i].author ,
        "titulo":datos[i].title ,
-       "descripcion":datos[i].description,
+       //"descripcion":datos[i].description,
        "url":datos[i].url 
    };
    console.log(datos)
@@ -85,19 +85,34 @@ endpoints.get('/media', async(req, res)=> {
     res.json({"mensaje":"no hay datos"})
  })
  
- endpoints.get('/fx', async(req, res)=> {
-    let busqueda=req.params.busqueda
-     const urlAPI = `http://api.mediastack.com/v1/news?access_key=574bd6512d018b2de9d6e833ebefd665`;
-     const respuestaAPI = await axios.get(urlAPI)
-     const datos = await respuestaAPI.data.data
- 
+ endpoints.get('/News/:busqueda/:lenguaje/:amount', async(req, res)=> {
      
-     console.log(datos)
- 
- 
-     if (datos && datos.length > 0){
- res.json({datos})
-     }
-     else
-     res.json({"mensaje":"no hay datos"})
+    let busqueda=req.params.busqueda
+    let lenguaje=req.params.lenguaje
+    let amount=req.params.amount
+
+     const urlAPI = 'https://newsapi.org/v2/everything?q='+busqueda+'&language='+lenguaje+'&sortBy=popularity&apiKey=3b67b1606d934062b206f3f9e56307fb';
+     const respuestaAPI = await axios.get(urlAPI)
+     const datos = await respuestaAPI.data.articles
+
+     let filter=[datos.length];
+     for(let i=0;i<amount;i++){  
+         if(i<datos.length){
+        filter[i]= {
+       //"author":datos[i].author ,
+       "titulo":datos[i].title ,
+       //"descripcion":datos[i].description,
+       "url":datos[i].url 
+   }
+};
+   console.log(datos)
+       }
+  
+   
+    if (datos && datos.length > 0)
+    {
+    res.json({"mensajes":filter})
+    }
+    else
+    res.json({"mensaje":"no hay datos"})
  })
